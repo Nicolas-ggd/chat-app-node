b69bf21aba1b09edb9d79990b20426f80ef7435b
Nicolas-ggd <nikusha.gogitidze.03@gmail.com>
2023-08-01 23:16:21 +0400

    update cors origins

--- index.js
+++ index.js
@@ -44,7 +44,7 @@ console.log(boxen(chalk.red.bgRed.bold("\n" + "Welcome to nicolas ggd server" + 
 const server = app.listen(process.env.PORT, console.log(`Server running on port ${process.env.PORT}`));
 const io = new Server(server, {
     cors: {
-        origin: 'http://localhost:5173',
+        origin: 'https://nicolas-ggd-chat.netlify.app',
         methods: ['GET', 'POST'],
     },
 })
