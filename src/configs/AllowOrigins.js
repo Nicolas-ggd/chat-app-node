30ecff9ecaa77d0661fba7cb3a41ea2977af9ddc
Nicolas-ggd <nikusha.gogitidze.03@gmail.com>
2023-08-01 23:26:53 +0400

    update allow origins

--- src/configs/AllowOrigins.js
+++ src/configs/AllowOrigins.js
@@ -1,5 +1,6 @@
 const AllowOrigins = [
     'https://nicolas-ggd-chat.netlify.app/',
+    'https://nicolas-ggd-chat.netlify.app',
     'http://localhost:8000/refresh',
     'http://localhost:8000/login',
     'https://chat-app-node-8ndm.onrender.com/refresh',
